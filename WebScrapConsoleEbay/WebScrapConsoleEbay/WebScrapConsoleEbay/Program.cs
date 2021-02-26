715d3077c9cbd27010655f45406178468b1b85ee
darrikt <dteller@eagles.ewu.edu>
2021-02-25 21:40:01 -0800

    Basic Web Scraper
    Console Based web scraper with hard coded ebay link

---
+++ WebScrapConsoleEbay/WebScrapConsoleEbay/WebScrapConsoleEbay/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace EbayScrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetHtmlAsync();
+            Console.ReadLine();
+        }
+        static async void GetHtmlAsync()
+        {
+            var url = "https://www.ebay.com/sch/i.html?_nkw=xbox+one&_in_kw=1&_ex_kw=&_sacat=0&LH_Complete=1&_udlo=&_udhi=&_samilow=&_samihi=&_sadis=15&_stpos=&_sargn=-1%26saslc%3D1&_salic=1&_sop=12&_dmd=1&_ipg=50";
+            var httpclient = new HttpClient();
+            var html = await httpclient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            var ProductsHtml = htmlDocument.DocumentNode.Descendants("ul")
+                .Where(node => node.GetAttributeValue("id", "")
+                    .Equals("ListViewInner")).ToList();
+
+            var ProductListItems = ProductsHtml[0].Descendants("li")
+                .Where(node => node.GetAttributeValue("id", "")
+                    .Contains("item")).ToList();
+            Console.WriteLine(ProductListItems.Count());
+            Console.WriteLine();
+            foreach( var ProductListItem in ProductListItems)
+            { 
+                /*Below the get attributes were taken from a downloaded sample of HTML from ebay*/
+            //ID
+                Console.WriteLine("Listing ID: " + ProductListItem.GetAttributeValue("listingid", ""));
+            //ProductName
+                Console.WriteLine("Product Name: " + ProductListItem.Descendants("h3")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("lvtitle")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
+            //Price               
+                Console.WriteLine("Price: " + ProductListItem.Descendants("li")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("lvprice prc")).FirstOrDefault().InnerText.Trim('\r','\n','\t')                    
+                    );
+            //ListingType
+                Console.WriteLine("Listing Type: " + ProductListItem.Descendants("li")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("lvformat")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t')
+                    );
+             
+                Console.WriteLine();
+            }       }    }}
