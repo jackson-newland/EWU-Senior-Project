94bbda790794f38f6d3f2a9609b3a0e5661a3d00
ChaseC1998 <61640111+ChaseC1998@users.noreply.github.com>
2021-04-16 11:54:48 -0700

    Update MainActivity.cs
    Still fixing the same issue.

--- GroceryApp/MainActivity.cs
+++ GroceryApp/MainActivity.cs
@@ -13,7 +13,7 @@ namespace GroceryApp
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.AddGroceryScreen);
+            SetContentView(Resource.Layout.activity_main);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
