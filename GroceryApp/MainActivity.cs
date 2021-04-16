2a3ef8728274ff3da8137c7a1d6a5a79d4c5bee9
Jackson Newland <jackson.newland@outlook.com>
2021-04-15 23:35:01 -0700

    Created: DeleteCouponScreen, SelectListScreen, SetListScreen, SetStoreScreen, backarrow.png.
    Changes:
    Resource.designer.cs
    GroceryApp.csproj
    MainActivity.cs (just changed the main screen)

--- GroceryApp/MainActivity.cs
+++ GroceryApp/MainActivity.cs
@@ -13,7 +13,7 @@ namespace GroceryApp
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.SetStoreScreen);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
