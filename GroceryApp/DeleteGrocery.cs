5409918bd44196c7e8a81faf866079fa7851fbba
ChaseC1998 <61640111+ChaseC1998@users.noreply.github.com>
2021-04-23 15:10:30 -0700

    Update DeleteGrocery.cs

--- GroceryApp/DeleteGrocery.cs
+++ GroceryApp/DeleteGrocery.cs
@@ -17,12 +17,18 @@ namespace GroceryApp
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Create your application here
 
             FindViewById<ImageButton>(Resource.Id.deleteGroceryBackButton).Click += (o, e) => SetContentView(Resource.Layout.activity_main);    //Setting view to activity_main.xml when back arrow button clicked on delete grocery screen.
 
             FindViewById<Button>(Resource.Id.deleteButtonDeleteGrocery).Click += (o, e) => SetContentView(Resource.Layout.activity_main);       //testing if issue with the imgbutton or the other screens.
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
