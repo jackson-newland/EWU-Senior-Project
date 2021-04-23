89e081d4bed56113cd910ad4120e7ca640aa96bc
ChaseC1998 <61640111+ChaseC1998@users.noreply.github.com>
2021-04-22 23:58:21 -0700

    Update MainActivity.cs

--- GroceryApp/MainActivity.cs
+++ GroceryApp/MainActivity.cs
@@ -15,10 +15,13 @@ namespace GroceryApp
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.activity_main); 
 
             FindViewById<Button>(Resource.Id.addButtonMain).Click += (o, e) => SetContentView(Resource.Layout.AddGroceryScreen);                //Setting view to AddGroceryScreen.xml when add button clicked on menu screen.
-            //Add events like the line above for the rest of the buttons.
+            FindViewById<Button>(Resource.Id.couponsButtonMain).Click += (o, e) => SetContentView(Resource.Layout.ViewCouponScreen);            //Setting view to ViewCouponScreen.xml when coupons button clicked on menu screen.
+            FindViewById<Button>(Resource.Id.deleteButtonMain).Click += (o, e) => SetContentView(Resource.Layout.DeleteGroceryScreen);          //Setting view to DeleteGroceryScreen.xml when delete button clicked on menu screen.
+            FindViewById<Button>(Resource.Id.setStoreButtonMain).Click += (o, e) => SetContentView(Resource.Layout.SetStoreScreen);             //Setting view to SetStoreScreen.xml when set store button clicked on menu.
+            FindViewById<Button>(Resource.Id.setListButtonMain).Click += (o, e) => SetContentView(Resource.Layout.SelectListScreen);            //Setting view to SelectListScreen.xml when set list button is clicked on menu.
 
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
