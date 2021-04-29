f66bd8fba2ba4520e99f4f612327bc418f5af809
ChaseC1998 <61640111+ChaseC1998@users.noreply.github.com>
2021-04-29 11:35:16 -0700

    Update DeleteGrocery.cs

--- GroceryApp/DeleteGrocery.cs
+++ GroceryApp/DeleteGrocery.cs
@@ -14,12 +14,39 @@ namespace GroceryApp
     [Activity(Label = "DeleteGrocery")]
     public class DeleteGrocery : Activity
     {
+
+        ImageButton backButton;
+        Button deleteButton, deleteAllButton;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.DeleteGroceryScreen);
 
-            FindViewById<ImageButton>(Resource.Id.deleteGroceryBackButton).Click += delegate { StartActivity(typeof(MainActivity)); };    //Setting view to activity_main.xml when back arrow button clicked on delete grocery screen.
+            backButton = FindViewById<ImageButton>(Resource.Id.deleteGroceryBackButton);                //Setting view to activity_main.xml when back arrow button clicked on delete grocery screen.
+            backButton.Click += OpenMain;
+
+            deleteButton = FindViewById<Button>(Resource.Id.deleteButtonDeleteGrocery);
+            deleteButton.Click += Delete;
+
+            deleteAllButton = FindViewById<Button>(Resource.Id.deleteAllButtonDeleteGrocery);
+            deleteAllButton.Click += DeleteAll;
+        }
+
+        public void OpenMain(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+        }
+
+        public void Delete(object sender, EventArgs e)
+        {
+            //Delete Grocery
+        }
+
+        public void DeleteAll(object sender, EventArgs e)
+        {
+            //Delete All Groceries
         }
 
     }
