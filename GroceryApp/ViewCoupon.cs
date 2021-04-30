41955d8c2fc6d6bf5c9e59b975bf37efb075245c
ChaseC1998 <61640111+ChaseC1998@users.noreply.github.com>
2021-04-30 12:33:37 -0700

    Almost done with ListView code in ViewCoupon
    Need to commit real quick cause I renamed the ListView in the AddGroceryScreen file.

--- GroceryApp/ViewCoupon.cs
+++ GroceryApp/ViewCoupon.cs
@@ -14,12 +14,40 @@ namespace GroceryApp
     [Activity(Label = "ViewCoupon")]
     public class ViewCoupon : Activity
     {
+        ImageButton backButton;
+        List<string> Items;
+        ListView ListViewViewCoupon;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.ViewCouponScreen);
-
-            FindViewById<ImageButton>(Resource.Id.viewCouponBackButton).Click += delegate { StartActivity(typeof(MainActivity)); };       //Setting view to activity_main.xml when back arrow button clicked on view coupon screen.
+            DisplayList();
+            backButton = FindViewById<ImageButton>(Resource.Id.viewCouponBackButton);                       //Setting view to activity_main.xml when back arrow button clicked on view coupon screen.
+            backButton.Click += OpenMain;
+            
         }
+
+        public void OpenMain(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+        }
+
+        public void DisplayList()                                               //Placeholder string content (duh). We will use this method to add the strings we make from the database to our ListView.
+        {
+            ListViewViewCoupon = FindViewById<ListView>(Resource.Id.listViewCoupons);
+
+            Items = new List<string>();
+            Items.Add("Item 1");
+            Items.Add("Item 2");
+            Items.Add("Item 3");
+            Items.Add("Item 4");
+            Items.Add("Item 5");
+            Items.Add("Item 6");
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, Items);
+            ListViewViewCoupon.Adapter = adapter;
+        }
+
     }
 }
