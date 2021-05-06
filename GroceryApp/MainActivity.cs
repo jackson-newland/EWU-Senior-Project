8006908698dde08bad55a45f6eadf45cc9def5e8
Jackson Newland <jackson.newland@outlook.com>
2021-05-05 20:10:02 -0700

    Created the Database and added a Grocery Class

--- GroceryApp/MainActivity.cs
+++ GroceryApp/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 using Android.Content;
 using Android.Views;
@@ -12,16 +13,27 @@ using System.Text;
 
 namespace GroceryApp
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "Home Screen", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        Button setListButton, setStoreButton, deleteCouponsButton, selectListButton;
+        Button setListButton, setStoreButton, deleteCouponsButton, selectListButton, deleteGroceryButton, addGroceryButton;
+        List<string> Items;
+        ListView ListViewMain;
+        GroceryAppDB _db;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            _db = new GroceryAppDB(); // Creates the connection to the database
+
+                                                                                                        //This is a test for creating and adding items to the list
+            _db.CreateTable("testList");                                                                // creates a list called testList
+            _db.AddGrocery("testList", "Banana", 3.50);                                                 // adds a banana for $3.50 to the current list 
+
+            DisplayList();                                                                              //Calls ListView displaying method.
 
             setListButton = FindViewById<Button>(Resource.Id.setListButtonMain);
             setListButton.Click += OpenSelectList;
@@ -32,10 +44,14 @@ namespace GroceryApp
             deleteCouponsButton = FindViewById<Button>(Resource.Id.couponsButtonMain);
             deleteCouponsButton.Click += OpenDeleteCoupon;
 
+            deleteGroceryButton = FindViewById<Button>(Resource.Id.deleteButtonMain);
+            deleteGroceryButton.Click += OpenDeleteGrocery;
 
+            addGroceryButton = FindViewById<Button>(Resource.Id.addButtonMain);
+            addGroceryButton.Click += OpenAddGrocery;
 
-           // FindViewById<Button>(Resource.Id.addButtonMain).Click += (o, e) => SetContentView(Resource.Layout.SetListScreen);
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -46,22 +62,55 @@ namespace GroceryApp
         public void OpenSelectList(object sender, EventArgs e)
         {
             Intent intent = new Intent(this, typeof(SelectList));
-           // SetContentView(Resource.Layout.SetListScreen);
             StartActivity(intent);
         }
 
         public void OpenSetSore(object sender, EventArgs e)
         {
             Intent intent = new Intent(this, typeof(SetStore));
-            // SetContentView(Resource.Layout.SetListScreen);
             StartActivity(intent);
         }
 
         public void OpenDeleteCoupon(object sender, EventArgs e)
         {
             Intent intent = new Intent(this, typeof(DeleteCoupon));
-            // SetContentView(Resource.Layout.SetListScreen);
             StartActivity(intent);
+        }
+
+        public void OpenDeleteGrocery(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(DeleteGrocery));
+            StartActivity(intent);
+        }
+
+        public void OpenAddGrocery(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(AddGrocery));
+            StartActivity(intent);
+        }
+
+        public void DisplayList()                                               //Placeholder string content (duh). We will use this method to add the strings we make from the database to our ListView.
+        {
+            ListViewMain = FindViewById<ListView>(Resource.Id.listViewMain);
+
+            Items = new List<string>();                                         //The code that populated the string list will change to concat strings using data from the database. Then it will be added
+           
+            IEnumerable<Grocery> list = _db.GetGroceries("testList");           // This method calls the current list and converts everything into a Ienumberable list
+            foreach (Grocery g in list)                                          // Goes through the list and adds each grocery name to the list
+            {
+                Items.Add(g.Name);
+
+            }
+
+            //Items.Add("Item 1");                                                //in the same way, except perhaps using a for loop or something to add all the items in a list to be displayed. May need to have
+            //Items.Add("Item 2");                                                //scrolling funtionality, but I will figure that out later.
+            //Items.Add("Item 3");
+            //Items.Add("Item 4");
+            //Items.Add("Item 5");
+            //Items.Add("Item 6");
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, Items);
+            ListViewMain.Adapter = adapter;
         }
     }
 }
