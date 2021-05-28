00d654f4dec651c8cb9bd05b9d46cb4ba96c55eb
Jackson Newland <jackson.newland@outlook.com>
2021-05-28 16:24:06 -0700

    Modfied the process of adding/selecting lists.

--- GroceryApp/DeleteGrocery.cs
+++ GroceryApp/DeleteGrocery.cs
@@ -14,13 +14,15 @@ namespace GroceryApp
     [Activity(Label = "DeleteGrocery")]
     public class DeleteGrocery : Activity
     {
-
         ImageButton backButton;
         Button deleteButton, deleteAllButton;
-        List<string> Items;
-        ListView ListViewDelGro;
+        List<string> Items, selectedItems;
+        ListView listViewDelGro;
+        TextView numSelectedDelGro;
         GroceryAppDB _db;
-        String currentList;
+        IEnumerable<Grocery> glist;
+        string currentList;
+        int selectCounter = 0;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -29,7 +31,8 @@ namespace GroceryApp
             currentList = "List1";                                                                       //The currentList string is the variable that will change when the user changes list. This string will be updated by that.
             DisplayList();                                                                               //Calls ListView displaying method.
 
-            backButton = FindViewById<ImageButton>(Resource.Id.deleteGroceryBackButton);                //Setting view to activity_main.xml when back arrow button clicked on delete grocery screen.
+            selectedItems = new List<string>();
+            backButton = FindViewById<ImageButton>(Resource.Id.deleteGroceryBackButton);                 //Setting view to activity_main.xml when back arrow button clicked on delete grocery screen.
             backButton.Click += OpenMain;
 
             deleteButton = FindViewById<Button>(Resource.Id.deleteButtonDeleteGrocery);
@@ -46,35 +49,64 @@ namespace GroceryApp
             StartActivity(intent);
         }
 
-        public void Delete(object sender, EventArgs e)
-        {
-            //Delete Grocery
+        public void Delete(object sender, EventArgs e)                                  //Delete Grocery method. Takes list of strings created from the ItemClick method below that is
+        {                                                                               //populated with strings equal to the groceries (in ToString form) that are clicked/tapped by
+            if (selectedItems != null)                                                  //user. If the user taps the grocery again and the list already contains it, it is removed.
+            {                                                                           //This foreach loop activates upon the delete button being clicked and goes through all of the
+                foreach (Grocery g in glist)                                            //groceries in the list and sees if they appear in their ToString form in the aformention list
+                {                                                                       //that keeps track of which items have been selected in the ListView.
+                    if (selectedItems.Contains(g.ToString()))
+                    {
+                        _db.DeleteGrocery(g.ID);
+                    }
+                }
+                numSelectedDelGro = FindViewById<TextView>(Resource.Id.itemsSelectedNumberDeleteGrocery);
+                numSelectedDelGro.Text = "0";
+                selectCounter = 0;
+                selectedItems.Clear();
+                DisplayList();
+            }
         }
 
         public void DeleteAll(object sender, EventArgs e)
         {
-            //Delete All Groceries. Changed it to use Jackson's helper method in the GroceryAppDB class.
-            _db.DeleteAllGrocery(currentList);
+            _db.DeleteAllGrocery(currentList);                                          //Delete All Groceries. Changed it to use Jackson's helper method in the GroceryAppDB class.
             DisplayList();
         }
 
-        public void DisplayList()                                   //Originally from MainActivity.cs. Will revert back if it doesn't work for some reason.
+        public void DisplayList()
         {
-            ListViewDelGro = FindViewById<ListView>(Resource.Id.listViewDeleteGrocery);
-
-            Items = new List<string>();                                           //The code that populated the string list will change to concat strings using data from the database. Then it will be added
-
-            IEnumerable<Grocery> glist = _db.GetGroceries("List1");               //This method calls the current list and converts everything into a Ienumberable list
-            foreach (Grocery g in glist)                                          // Goes through the list and adds each grocery name to the list
+            listViewDelGro = FindViewById<ListView>(Resource.Id.listViewDeleteGrocery);
+            Items = new List<string>();
+            glist = _db.GetGroceries("List1");                                          //This method calls the current list and converts everything into a Ienumberable list.
+            foreach (Grocery g in glist)                                                //Goes through the list and adds each grocery in format to the list.
             {
                 Items.Add(g.ToString());
-
             }
 
             ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, Items);
-            ListViewDelGro.Adapter = adapter;
+            listViewDelGro.Adapter = adapter;
+            listViewDelGro.ItemClick += ListViewDelGro_ItemClick;
         }
 
+        private void ListViewDelGro_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            string selectedGroc = Items[e.Position];
+            numSelectedDelGro = FindViewById<TextView>(Resource.Id.itemsSelectedNumberDeleteGrocery);               //THE COUNTER CODE IS BEING TESTED RIGHT NOW
+
+            if (selectedItems.Contains(selectedGroc))
+            {
+                selectedItems.Remove(selectedGroc);
+                selectCounter--;
+                numSelectedDelGro.Text = selectCounter.ToString();
+            }
+            else
+            {
+                selectedItems.Add(selectedGroc);
+                selectCounter++;
+                numSelectedDelGro.Text = selectCounter.ToString();
+            }
+        }
 
     }
 }
--- GroceryApp/SelectList.cs
+++ GroceryApp/SelectList.cs
@@ -16,20 +16,19 @@ namespace GroceryApp
     {
 
         ImageButton backButton;
-        Button addButton, deleteButton;
-
+        Button addButton, deleteButton, setButton;
         TextView currentList;
         List<string> Items;
         ListView listSelectList;
         GroceryAppDB _db;
+        int requestCodeSetList = 7;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.SelectListScreen);
             _db = new GroceryAppDB();
-
-            DisplayList();
+            Items = new List<string>();
 
             backButton = FindViewById<ImageButton>(Resource.Id.slBackButton);
             backButton.Click += OpenMain;
@@ -39,33 +38,64 @@ namespace GroceryApp
 
             deleteButton = FindViewById<Button>(Resource.Id.slDeleteButton);
             deleteButton.Click += DeleteList;
+
+            setButton = FindViewById<Button>(Resource.Id.slSetButton);
+            setButton.Click += SetButton_Click;
+
+            listSelectList = FindViewById<ListView>(Resource.Id.slLists);
+            listSelectList.ItemClick += ListSelectList_ItemClick;
+
+            currentList = FindViewById<TextView>(Resource.Id.slCurrentListInfo);
+
+            DisplayList();
+        }
+
+        private void ListSelectList_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            currentList.Text = Items[e.Position];
+        }
+
+        private void SetButton_Click(object sender, EventArgs e)
+        {
+            if(currentList.Text.ToString() == "Select A List")
+            {
+                OpenMain(sender, e);
+            } else
+            {
+                Intent data = new Intent();
+                data.SetData(Android.Net.Uri.Parse(currentList.Text.ToString()));
+                SetResult(Result.Ok, data);
+                Finish();
+            }
+           
         }
 
         public void OpenMain(object sender, EventArgs e)
         {
-            Intent intent = new Intent(this, typeof(MainActivity));
-
-            StartActivity(intent);
+          
+            Intent data = new Intent();
+            data.SetData(Android.Net.Uri.Parse(currentList.Text.ToString()));
+          
+            SetResult(Result.Canceled, data);
+            Finish();
         }
 
         public void AddList(object sender, EventArgs e)
         {
-            Intent intent = new Intent(this, typeof(SetList));
-
-            StartActivity(intent);
+            StartActivityForResult(typeof(SetList), requestCodeSetList);
+         
         }
 
         public void DeleteList(object sender, EventArgs e)
         {
-            _db.DeleteList("List1"); // testing for deletion of list
+            _db.DeleteList(currentList.Text.ToString());
+            currentList.Text = "Select A List";
+            DisplayList();
         }
 
         public void DisplayList()
         {
-            listSelectList = FindViewById<ListView>(Resource.Id.slLists);
-
-
-            Items = new List<string>();
+                    
             IEnumerable<GroceryLists> list = _db.GetList();
             foreach (GroceryLists t in list)
             {
@@ -77,5 +107,20 @@ namespace GroceryApp
             listSelectList.Adapter = adapter;
         }
 
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+           if(requestCode == requestCodeSetList)
+            {
+                if(resultCode == Result.Ok)
+                {
+                    currentList.Text = data.Data.ToString();
+                    DisplayList();
+                } 
+                else if(resultCode == Result.Canceled)
+                {
+                    DisplayList();
+                }
+            }
+        }
     }
 }
